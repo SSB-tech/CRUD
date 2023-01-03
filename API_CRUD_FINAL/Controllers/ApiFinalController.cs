8cee91109b60a77f336b2c197c661b502bcca599
Sushant <basnetsushantsingh@gmail.com>
2023-01-03 14:30:15 +0545

    CRUD operation using LINQ

---
+++ API_CRUD_FINAL/Controllers/ApiFinalController.cs
@@ -0,0 +1,55 @@
+﻿using API_CRUD_FINAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_CRUD_FINAL.Controllers
+{
+	
+	public class ApiFinalController : ApiController
+    {
+		public api_crud_final_dbEntities ob = new api_crud_final_dbEntities();
+		[HttpGet]
+		public IHttpActionResult getemp()
+		{
+			List<apitbl> data = ob.apitbls.ToList();
+			return Ok(data);
+		}
+
+		[HttpPost]
+		public IHttpActionResult empcreate(apitbl emp)
+		{
+			ob.apitbls.Add(emp);
+			ob.SaveChanges();
+			return Ok();
+		}
+
+		[HttpGet]
+		public IHttpActionResult empretrv(int id)
+		{
+			var ret = ob.apitbls.Where(model => model.id == id).FirstOrDefault();
+			ob.SaveChanges();
+			return Ok(ret);
+		}
+
+		[HttpPut]
+		public IHttpActionResult empedit(apitbl eestore)
+		{
+			ob.Entry(eestore).State= System.Data.Entity.EntityState.Modified;
+			ob.SaveChanges();
+			return Ok();
+		}
+
+		[HttpDelete]
+		public IHttpActionResult empdel(int id)
+		{
+			var empdelt = ob.apitbls.Where(model => model.id==id).FirstOrDefault();
+			ob.Entry(empdelt).State = System.Data.Entity.EntityState.Deleted;
+			ob.SaveChanges();
+			return Ok();
+		}
+	}
+}
---
+++ API_CRUD_FINAL/Controllers/MvcFinalController.cs
@@ -0,0 +1,153 @@
+﻿using API_CRUD_FINAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using System.Web.WebSockets;
+
+namespace API_CRUD_FINAL.Controllers
+{
+    public class MvcFinalController : Controller
+    {
+        HttpClient client = new HttpClient();
+        // GET: MvcFinal
+        public ActionResult Index()
+        {
+            List<apitbl> emp_details = new List<apitbl>();
+            client.BaseAddress = new Uri("https://localhost:44342/api/ApiFinal");
+
+            var response = client.GetAsync("ApiFinal");
+            response.Wait();
+            var test = response.Result;
+
+          if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<apitbl>>();
+				display.Wait();
+                emp_details = display.Result;
+			}
+            return View(emp_details);
+        }
+
+		//create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(apitbl e)
+        {
+			client.BaseAddress = new Uri("https://localhost:44342/api/ApiFinal");
+
+			var response = client.PostAsJsonAsync("ApiFinal",e);
+			response.Wait();
+			var test = response.Result;
+
+			if (test.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+            return Create();
+        }
+
+		//Detail or Retrieving selective data
+        public ActionResult Details(int id)
+        {
+			client.BaseAddress = new Uri("https://localhost:44342/api/ApiFinal");
+
+            apitbl empret= new apitbl();    
+
+			var response = client.GetAsync("ApiFinal?id=" + id.ToString());
+			response.Wait();
+			var test = response.Result;
+
+			if (test.IsSuccessStatusCode)
+			{
+				var display = test.Content.ReadAsAsync<apitbl>();
+                display.Wait();
+                empret = display.Result;
+			}
+			return View(empret);
+		}
+
+		//Edit
+        public ActionResult Edit(int id)
+        {
+			client.BaseAddress = new Uri("https://localhost:44342/api/ApiFinal");
+
+			apitbl empret = new apitbl();
+
+			var response = client.GetAsync("ApiFinal?id=" + id.ToString());
+			response.Wait();
+			var test = response.Result;
+
+			if (test.IsSuccessStatusCode)
+			{
+				var display = test.Content.ReadAsAsync<apitbl>();
+				display.Wait();
+				empret = display.Result;
+			}
+			return View(empret);
+		}
+
+		[HttpPost]
+		public ActionResult Edit(apitbl estore)
+		{
+			client.BaseAddress = new Uri("https://localhost:44342/api/ApiFinal");
+
+			var response = client.PutAsJsonAsync("ApiFinal", estore);
+			response.Wait();
+			var test = response.Result;
+
+			if (test.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		//Delete
+
+		[HttpGet]
+		public ActionResult Delete(int id)
+		{
+			client.BaseAddress = new Uri("https://localhost:44342/api/ApiFinal");
+
+			apitbl empret = new apitbl();
+
+			var response = client.GetAsync("ApiFinal?id=" + id.ToString());
+			response.Wait();
+			var test = response.Result;
+
+			if (test.IsSuccessStatusCode)
+			{
+				var display = test.Content.ReadAsAsync<apitbl>();
+				display.Wait();
+				empret = display.Result;
+			}
+			return View(empret);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public ActionResult Deleteconfirm(int id)
+		{
+			client.BaseAddress = new Uri("https://localhost:44342/api/ApiFinal");
+
+			var response = client.DeleteAsync("ApiFinal?id=" + id.ToString());
+			response.Wait();
+
+			var test = response.Result;
+
+			if (test.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View("Delete");
+		}
+
+	}
+}
